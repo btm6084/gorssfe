170d818d15f6598b529f26b0fe94e5f71c2d2e85
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-01-08 14:54:23 -0600

    Add onunlaod to scroll top so that we start at the beginning.

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ function HomePage({ feed }) {
 	// Effectively componentDidMount
 	React.useEffect(() => {
 		window.setTimeout(() => { window.scrollTo(0, 0); }, 250);
+		window.addEventListener("unload", function(event) { window.scrollTo(0, 0); });
 	})
 
 	const serverHost = 'https://dailies.benjaminmaynor.com/api';
